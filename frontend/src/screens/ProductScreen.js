45b21dc0725f15ad3dedb8f070b80914c3b1e727
yuikim <nokdoo00@gmail.com>
2021-03-18 22:02:59 -0400

    ListGroup for adding details of each product

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,7 +9,35 @@ function ProductScreen({match}) {
   const product = products.find(p => p._id === match.params.id)
   return (
     <div>
-      {product.name}
+      <Link to="/" className="btn btn-light my-3">Go Back</Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid/>
+        </Col>
+        
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'}/>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Price: ${product.price}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Description: {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+        
+        </Col>
+      </Row>
     </div>
   )
 }
