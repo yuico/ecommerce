ef4a10e68bb0747c5fe4a64570a60c6249c09814
yuikim <nokdoo00@gmail.com>
2021-03-23 20:44:00 -0400

    add routes by adding routes array of urls in views of base

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+def getRoutes(request):
+  routes = [
+    '/api/admin',
+    '/api/product'
+  ]
+  return JsonResponse(routes, safe=False)
