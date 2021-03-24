7c5e1efc218cd1745741c72538b486ad1fd447fe
yuikim <nokdoo00@gmail.com>
2021-03-24 00:13:23 -0400

    install restframwork and set for api-view

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .products import products
 
 
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
   routes = [
     '/api/products',
     '/api/proucts/<id>/reviews',
     '/api/proucts/<id>',
   ]
-  return JsonResponse(routes, safe=False)
+  return Response(routes)
 
 def getProducts(request):
   return JsonResponse(products, safe=False)
