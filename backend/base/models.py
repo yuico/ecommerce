93f246ccd902e07618048971d2bccdc787bba9a6
yuikim <nokdoo00@gmail.com>
2021-03-25 19:36:29 -0400

    add OrderItem and ShippingAddrss model

--- backend/base/models.py
+++ backend/base/models.py
@@ -47,3 +47,27 @@ class Order(models.Model):
 
   def __str__(self):
     return str(self.createdAt)
+
+class OrderItem(models.Model):
+  order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+  product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+  name = models.CharField(max_length=200, null=True, blank=True)
+  qty = models.IntegerField(null=True, blank=True, default=0)
+  price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+  image = models.CharField(max_length=200, null=True, blank=True)
+  _id = models.AutoField(primary_key = True, editable=False)
+
+  def __str__(self):
+    return self.name
+
+class ShippingAddress(models.Model):
+  order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+  address = models.CharField(max_length=200, null=True, blank=True)
+  city = models.CharField(max_length=200, null=True, blank=True)
+  postalCode = models.CharField(max_length=200, null=True, blank=True)
+  country = models.CharField(max_length=200, null=True, blank=True)
+  shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+  _id = models.AutoField(primary_key = True, editable=False)
+
+  def __str__(self):
+    return self.address
