af297fac6c5cbab3b7ecc0f458efc893e095646c
yuikim <nokdoo00@gmail.com>
2021-03-25 20:25:49 -0400

    add serializer and modify getProducts fun

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 
 from .models import Product
 from .products import products
+from .serializers import ProductSerializer 
 
 
 # Create your views here.
@@ -12,16 +13,22 @@ from .products import products
 @api_view(['GET'])
 def getRoutes(request):
   routes = [
-    '/api/products',
-    '/api/proucts/<id>/reviews',
-    '/api/proucts/<id>',
+    '/api/products/',
+    '/api/products/create/',
+    '/api/proucts/upload/',
+    '/api/proucts/<id>/reviews/',
+
+    '/api/proucts/top/',
+    
+    '/api/proucts/<updata>/<id>/',
   ]
   return Response(routes)
 
 @api_view(['GET'])
 def getProducts(request):
   products = Product.objects.all()
-  return Response(products)
+  serializers = ProductSerializer(products, many=True)
+  return Response(serializers.data)
 """ def getProducts(request):
   return JsonResponse(products, safe=False) """
 
